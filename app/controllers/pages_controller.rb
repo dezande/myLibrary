c1c25b82ffc57861fe6b7c9fba84ef8d32978c08
Guilaine Ghossoub <guilaine.ghossoub4@gmail.com>
2017-04-07 17:18:22 +0200

    users controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @users = User.all
+    @books = Book.all
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,41 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+    @user = User.new(users_params)
+    @user.save
+    redirect_to :back
+  end
+
+  def update
+    @user.update(users_params)
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to :back, notice: 'User was successfully deleted.'
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:first_name, :last_name, :email, :photo)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
