f8984228d326e71a7506c0a7d358d51f1642aea0
Guilaine Ghossoub <guilaine.ghossoub4@gmail.com>
2017-04-07 23:46:01 +0200

    books page with controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,56 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(books_params)
+    if @book.save
+      redirect_to books_path #, notice: 'Book was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @book.update(books_params)
+      redirect_to books_path #, notice: 'Book was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book.destroy if can_delete_book
+    redirect_to books_path #, notice: 'Book was successfully deleted.'
+  end
+
+  private
+
+  def books_params
+    params.require(:book).permit(:title, :author, :summary, :photo, :status)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def can_delete_book
+    if @book.status == 'available'
+      true
+    else
+      false
+    end
+  end
 end
